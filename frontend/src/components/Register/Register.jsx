6ca97744eb0f1d0b21e25a4f0fb404d6eccd7801
allbutgold <contact@leokieffer.com>
2023-05-09 09:15:23 +0200

    add Register cmponent

---
+++ frontend/src/components/Register/Register.jsx
@@ -0,0 +1,11 @@
+import styles from './Register.module.scss';
+
+const Register = () => {
+  return ( 
+    <section className={styles.Register}>
+      <h1>Register</h1>
+    </section>
+  );
+}
+
+export default Register;
