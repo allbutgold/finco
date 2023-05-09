3adbc14029344e6ea00d72743b3142ef2e734616
allbutgold <contact@leokieffer.com>
2023-05-09 11:37:28 +0200

    add RegisterForm component and register route

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import multer from "multer";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import { getDb } from "./utils/db.js";
 
 
 const server = express();
@@ -12,7 +13,7 @@ const PORT = process.env.PORT;
 
 // * ===== BODY PARSER ======
 // enabling cors
-server.use(cors({ origin: process.env.VITE_FRONTEND, credentials: true }));
+server.use(cors({ origin: true, credentials: true }));
 // for JSON
 server.use(express.json());
 // for cookies
@@ -23,6 +24,14 @@ server.use(cookieParser());
 
 // room for routes
 server.get("/",(req,res)=>{res.send("Hello,world")})
+
+server.post('/register' , async (req,res)=> {
+  const db = await getDb();
+  const { username, password, email, agreedToTnC } = req.body;
+  const result = await db.collection('finco').insertOne(req.body);
+  res.json(result);
+})
+
 
 // * ===== LOGGER ======
 server.use(morgan("dev"));
