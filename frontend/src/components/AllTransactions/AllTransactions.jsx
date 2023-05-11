b642186438bf165796117680e29831e968b2df2f
allbutgold <contact@leokieffer.com>
2023-05-11 10:35:13 +0200

    add TransactionList to Report page

---
+++ frontend/src/components/AllTransactions/AllTransactions.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { userStore } from "../../utils/userStore.js";
+
+
+const TransactionList = () => {
+  const [transactions, setTransactions] = useState([]);
+  const userID = userStore((state) => state.userID);
+
+  const URL = import.meta.env.VITE_BACKEND_URL;
+
+  useEffect(() => {
+    const getTransactions = async () => {
+
+      const response = await fetch(URL + 'getAllTransactions?id=' + userID, {
+        credentials: 'include',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const data = await response.json();
+      setTransactions(data);
+    }
+    getTransactions();
+  }, []) 
+
+  return (
+    <section>
+      <article>
+        <div style={{ padding: '40px' }}>
+          {Object.entries(transactions)
+            .flatMap(([key, array]) => array)
+            .map((transaction, index) => (
+              <div style={{ padding: '20px' }} key={index}>
+                <p>{transaction.category}</p>
+                <p className={transaction.type === 'expense' ? 'red' : 'green'}>{transaction.amount}</p>
+                <p>{transaction.date}</p>
+              </div>
+            ))}
+        </div>
+      </article>
+    </section>
+  );
+}
+
+export default TransactionList;
