165ebe2b606467b7b552a11b713018260828988a
Hanni <ngynngchn@gmail.com>
2023-05-15 09:36:28 +0200

    changed icons to emojis

--- frontend/src/components/TransactionList/SingleTransaction.jsx
+++ frontend/src/components/TransactionList/SingleTransaction.jsx
@@ -1,11 +1,14 @@
 import styles from "./TransactionList.module.scss";
-import { formatToDollar, mapImage } from "../../utils/helper.js";
+import { formatToDollar, mapType, mapColor } from "../../utils/helper.js";
 
 function SingleTransaction({ transaction }) {
 	return (
 		<div className={styles.SingleTransaction}>
-			<div className={styles.TransactionImage}>
-				<img src={mapImage(transaction)} alt={transaction.category} />
+			<div
+				className={styles.TransactionImage}
+				style={{ backgroundColor: "var(--bg-100)" }}>
+				{/* <img src={mapType(transaction,icon)} alt={transaction.category} /> */}
+				<h3>{mapType(transaction, "emoji")}</h3>
 			</div>
 			<div className={styles.TransactionDetails}>
 				<h4>{transaction.category}</h4>
@@ -14,8 +17,10 @@ function SingleTransaction({ transaction }) {
 					<p>{transaction.date}</p>
 				</div>
 			</div>
-			<p className={transaction.type === "expense" ? "red" : "green"}>
-				{formatToDollar(transaction.amount)}
+			<p className={transaction.type === "expense" ? "negative" : "positive"}>
+				{transaction.type === "expense"
+					? "-" + formatToDollar(transaction.amount)
+					: "+" + formatToDollar(transaction.amount)}
 			</p>
 		</div>
 	);
