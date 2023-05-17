330a9628a5b3c649379c42e9a7b535b532aabd9b
Hanni <ngynngchn@gmail.com>
2023-05-17 11:22:48 +0200

    changed setup account

--- frontend/src/pages/Setup/Setup.jsx
+++ frontend/src/pages/Setup/Setup.jsx
@@ -1,11 +1,15 @@
 import styles from "./Setup.module.scss";
 import SetupForm from "../../components/SetupForm/SetupForm.jsx";
 import Header from "../../components/Header/Header";
+import { userStore } from "../../utils/userStore.js";
 
 const Setup = () => {
+	const userPic = userStore.getState().userPic;
 	return (
 		<section className={styles.Setup}>
-			<Header title="Setup your account" />
+			<Header
+				title={userPic !== null ? "Edit your account" : "Setup your account"}
+			/>
 			<SetupForm />
 		</section>
 	);
